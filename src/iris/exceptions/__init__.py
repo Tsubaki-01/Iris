06aed7549ee76f5eec43a66989c865bba18ddf34
Weng, Qiang <92919224+Tsubaki-01@users.noreply.github.com>
2026-05-03 00:47:59 +0800

    「chore」：修改了包导出逻辑

--- src/iris/exceptions/__init__.py
+++ src/iris/exceptions/__init__.py
@@ -1,29 +1,17 @@
-from typing import TYPE_CHECKING, Any
-
-if TYPE_CHECKING:
-    from .exceptions import (
-        IrisConfigError,
-        IrisError,
-        IrisExecutionError,
-        IrisParserError,
-        IrisToolError,
-        IrisValidationError,
-    )
-
+from .exceptions import (
+    IrisConfigError,
+    IrisError,
+    IrisExecutionError,
+    IrisParserError,
+    IrisToolError,
+    IrisValidationError,
+)
 
 __all__ = [
-    "IrisError",
     "IrisConfigError",
+    "IrisError",
+    "IrisExecutionError",
+    "IrisParserError",
     "IrisToolError",
     "IrisValidationError",
-    "IrisParserError",
-    "IrisExecutionError",
 ]
-
-
-def __getattr__(name: str) -> Any:
-    if name in __all__:
-        from . import exceptions as _exceptions
-
-        return getattr(_exceptions, name)
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
